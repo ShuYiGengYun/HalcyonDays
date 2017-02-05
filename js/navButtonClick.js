15891a02d2863216fd7284807bfe3e9525258672
zzy <917293906@qq.com>
2017-02-05 19:59:47 +0800

    switch nav button

---
+++ js/navButtonClick.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Shinelon on 2017/2/5.
+ */
+$(function () {
+    $("#nav-toggle").click(function () {
+        /*显示导航-start*/
+        $("nav nav").toggleClass("showToggle");
+        /*显示导航-end*/
+
+
+        /*导航按钮切换-start*/
+        var klass = $(this).find("i").attr("class");
+        var index = klass.indexOf("icon-remove");
+        var index_1 = klass.indexOf("icon-reorder");
+        if (index < 0) {
+            $(this).find("i").addClass("icon-remove").removeClass("icon-reorder");
+        }
+        if (index_1 < 0) {
+            $(this).find("i").addClass("icon-reorder").removeClass("icon-remove");
+        }
+        /*导航按钮切换-end*/
+
+
+    });
+});
